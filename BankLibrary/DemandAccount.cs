9dffbecf4d3dd94955ede728163ca287d3e21fa0
salex42 <lexgml@gmail.com>
2019-09-02 21:41:19 +0700

    Add project files.

---
+++ BankLibrary/DemandAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public class DemandAccount : Account
+    {
+        public DemandAccount(decimal sum, int percentage) : base(sum, percentage)
+        {
+        }
+
+        protected internal override void Open()
+        {
+            base.OnOpened(new AccountEventArgs("Открыт новый счет до востребования! Id счета: " + this._id, this._sum));
+        }
+    }
+}
---
+++ BankLibrary/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public interface IAccount
+    {
+
+        // put money
+        void Put (decimal sum);
+        
+        //get money
+        decimal Withdraw(decimal sum);
+    }
+}
